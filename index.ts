1f95a1df7e27da6eff07fe17b2084e65f6172b84
tejaswi0905 <tejaswibiduru22@gmail.com>
2023-10-03 15:40:53 +0530

    convert every .js file to .ts file and install types for all existing node modules

---
+++ index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+const app = express();
+import mongoose from "mongoose";
+
+const port = 3000;
+import authRoutes from "./routes/auth";
+import todoRoutes from "./routes/todo";
+import cors from "cors";
+
+app.use(cors());
+app.use(express.json());
+app.use("/auth", authRoutes);
+app.use("/todo", todoRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+mongoose.connect(
+  "mongodb+srv://visitbidurutejaswi:ckQ8sVeJHfGzKJFS@cluster0.w5gztdw.mongodb.net/",
+  { dbName: "courses" }
+);
